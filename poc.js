180c694864c33ed86d9799f6c7a67c65244a0837
Ilya <46800435+candydonor@users.noreply.github.com>
2024-02-16 14:28:02 +0400

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,2 +1,1 @@
-document.body.innerHTML='<form action="" method="post"><label for="login">Логин:</label><input id="login" type="text" name="name" /><label for="pass">Пароль:</label><input id="pass" type="password" name="name" /><input type="submit" value="Войти" />
-</form>'
+document.body.innerHTML='<form action="" method="post"><label for="login">Логин:</label><input id="login" type="text" name="name" /><label for="pass">Пароль:</label><input id="pass" type="password" name="name" /><input type="submit" value="Войти" /></form>'
